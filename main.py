ff83c365dc86cb562fe6d4725be9a56d772beb79
Snehal Thakur <snehal.hadoop@gmail.com>
2022-08-25 23:32:01 +0530

    Weather app initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+
+# import json to load JSON data to a python dictionary
+import json
+
+# urllib.request to make a request to api
+import urllib.request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def weather():
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        # for default name mathura
+        city = 'pune'
+
+    # your API key will come here
+    api = "ce2e089bb7bbcaa2e15edf51422cea6b"
+
+    # source contain json data from api
+    source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api).read()
+
+    # https: // api.openweathermap.org / data / 2.5 / weather?q = {city name} & appid = {API key}
+    # converting JSON data to a dictionary
+    list_of_data = json.loads(source)
+
+    # data for variable list_of_data
+    data = {
+        "country_code": str(list_of_data['sys']['country']),
+        "city_name": str(list_of_data['name']),
+        "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+        "temp": str(list_of_data['main']['temp']) + 'k',
+        "temp_cel": str(list_of_data['main']['temp']-273.15) + 'c',
+        "pressure": str(list_of_data['main']['pressure']),
+        "humidity": str(list_of_data['main']['humidity']),
+    }
+    print(data)
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
